bb03d7b52419902f60505466d7830e6c7b61785e
Andrey Olishchuk <bobroe.web@gmail.com>
2022-12-30 00:11:10 +0100

    Supabase chart to Argo

--- modules/argorocket/main.tf
+++ modules/argorocket/main.tf
@@ -6,12 +6,31 @@ resource "helm_release" "argocd" {
   depends_on = [kubernetes_namespace.argocd]
 }
 
-resource "helm_release" "supabase" {
-  name       = "supabase"
-  namespace  = "supabase"
-  repository = "https://supabase-community.github.io/supabase-kubernetes"
-  chart      = "supabase"
-  depends_on = [kubernetes_namespace.supabase]
+resource "kubectl_manifest" "argoapp1" {
+  depends_on = [helm_release.argocd]
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: supabase
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://supabase-community.github.io/supabase-kubernetes
+    targetRevision: HEAD
+    chart: supabase
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: supabase
+  syncPolicy:
+    syncOptions:
+    - CreateNamespace=true
+    automated:
+      selfHeal: true
+      prune: true 
+      allowEmpty: false 
+YAML
 }
 
 resource "kubernetes_namespace" "argocd" {
